36834fb6128b742b65dba235663118b16c648f69
Dustin Yochim <yochim.dustin@gmail.com>
2023-01-30 16:47:43 -0600

    Add buttons for changing mode and resetting grid

--- js/script.js
+++ js/script.js
@@ -1,21 +1,73 @@
-let grid_size = 16;
+let gridSize = 16;
+let currentMode = 'black';
 
-const container = document.querySelector('.container');
+const container = document.querySelector('.grid-container');
 
-for (let rows = 0; rows < grid_size; rows++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-    for (let square = 0; square < grid_size; square++) {
-        const square = document.createElement('div');
-        square.classList.add('gridItem');
-        row.appendChild(square);
+
+
+
+function buildGrid() {
+    for (let rows = 0; rows < gridSize; rows++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let square = 0; square < gridSize; square++) {
+            const square = document.createElement('div');
+            square.classList.add('grid-item');
+            row.appendChild(square);
+        }
     }
 }
 
-const gridItems = document.querySelectorAll('.gridItem');
-gridItems.forEach((gridItem) => {
-    gridItem.addEventListener('mouseover', () => {
-        gridItem.setAttribute('style', 'background: black;');
+function destroyGrid() {
+    for (let rows = 0; rows < gridSize; rows++) {
+        let row = document.querySelector('.row');
+        for (let square = 0; square < gridSize; square++) {
+            let square = document.querySelector('.grid-item');
+            row.removeChild(square);
+        }
+        container.removeChild(row);
+    }
+}
+
+function changeGridSize() {
+    destroyGrid();
+    gridSize = prompt('Enter a new size for the grid between 2 - 100.');
+    buildGrid();
+    addListeners();
+}
+
+function resetGrid() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((gridItem) => {
+        gridItem.setAttribute('style', 'background: white;');
     });
-});
+}
+
+function addListeners() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((gridItem) => {
+        gridItem.addEventListener('mouseover', () => {
+            if (currentMode === 'black') {
+                gridItem.setAttribute('style', 'background: black');
+            } else if (currentMode === 'rainbow') {
+                gridItem.setAttribute('style', `background: #${Math.floor(Math.random()*16777215).toString(16)};`)
+            } else {
+                gridItem.setAttribute('style', 'background: white;');
+            }
+        });
+    });
+}
+
+function changeColorMode(color) {
+    if (color === 'black') {
+        currentMode = 'black';
+    } else if (color === 'rainbow') {
+        currentMode = 'rainbow';
+    } else {
+        currentMode = 'erase';
+    }
+}
+
+buildGrid(16);
+addListeners();
